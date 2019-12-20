419462f6f082a523db1381aa30d0b287b726ca13
Raien Li <v-raienl@microsoft.com>
2019-12-20 18:34:05 +0800

    recreate RPMovies project

---
+++ aspdotnetcore/webapps/razorpages/RPMovies/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RPMovies.Data;
+using RPMovies.Models;
+
+namespace RPMovies.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RPMovies.Data.RPMoviesContext _context;
+
+        public IndexModel(RPMovies.Data.RPMoviesContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movies.ToListAsync();
+        }
+    }
+}
