67ee1fd7fddcb6cc183d95e35763126e0d2adecd
Raien Li <v-raienl@microsoft.com>
2019-12-21 15:24:04 +0800

    update RPMovies project

--- aspdotnetcore/webapps/razorpages/RPMovies/Program.cs
+++ aspdotnetcore/webapps/razorpages/RPMovies/Program.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RPMovies.Models;
 
 namespace RPMovies
 {
@@ -13,7 +15,24 @@ namespace RPMovies
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
