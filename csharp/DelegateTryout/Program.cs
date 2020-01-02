b9bc24ccae2424295a843d9281e14544dff758ec
Raien Li <v-raienl@microsoft.com>
2020-01-02 15:47:35 +0800

    add proj

---
+++ csharp/DelegateTryout/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DelegateTryout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DTryout dt = new DTryout();
+            dt.DoSomething();
+        }
+    }
+
+    public class DTryout
+    {
+        public delegate int Comparison<in T>(T left, T right);
+
+        private int CompareLength(string left, string right)
+        {
+            if (left.Length < right.Length)
+            {
+                return -1;
+            }
+            else if (left.Length == right.Length)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        private int CompareLenthRev(string left, string right)
+        {
+            if (left.Length < right.Length)
+            {
+                return 1;
+            }
+            else if (left.Length == right.Length)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        private List<T> MySort<T>(List<T> sourceList, Comparison<T> comparer)
+        {
+            for (int i = 0; i < sourceList.Count; i++)
+            {
+                for (int j = i + 1; j < sourceList.Count; j++)
+                {
+                    if (comparer(sourceList[i], sourceList[j]) == 1)
+                    {
+                        var temp = sourceList[j];
+                        sourceList[j] = sourceList[i];
+                        sourceList[i] = temp;
+                    }
+                }
+            }
+
+            return sourceList;
+        }
+
+        public void DoSomething()
+        {
+            Comparison<string> comparer = CompareLength;
+            Comparison<string> comp2 = CompareLenthRev;
+            
+            List<string> strList = new List<string>()
+            {
+                "this",
+                "on",
+                "a",
+                "something",
+                "yes"
+            };
+
+            foreach (var s in strList)
+            {
+                Console.WriteLine(s);
+            }
+
+            var ret = MySort(strList, comparer);
+
+            foreach (var s in ret)
+            {
+                Console.WriteLine(s);
+            }
+
+            var ret2 = MySort(strList, comp2);
+
+            foreach (var s in ret)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ csharp/IteratorTryout/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IteratorTryout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in GetSingleDigitNumbers())
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static IEnumerable<int> GetSingleDigitNumbers()
+        {
+            yield return 1;
+            yield return 3;
+            yield return 4;
+        }
+    }
+}
