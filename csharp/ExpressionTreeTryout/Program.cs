d760fb03486d4c0f7a08e577c843119967dc84f8
Raien Li <v-raienl@microsoft.com>
2020-01-03 22:05:33 +0800

    add proj

---
+++ csharp/ExpressionTreeTryout/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ExpressionTreeTryout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression<Func<int, int>> addFive = (num) => num + 5;
+
+            if (addFive.NodeType == ExpressionType.Lambda)
+            {
+                var lambdaExp = (LambdaExpression)addFive;
+                var parameter = lambdaExp.Parameters.First();
+
+                Console.WriteLine(parameter.Name);
+                Console.WriteLine(parameter.Type);
+            }
+
+            var one = Expression.Constant(1, typeof(int));
+            var two = Expression.Constant(2, typeof(int));
+            var addition = Expression.Add(one, two);
+        }
+    }
+}
